756fcb4348a3f61a1f3a6e01fcb831b89584296a
hemantfoundations <mishrahemantkumar777@gmail.com>
2026-01-17 22:28:38 +0530

    basics.py

---
+++ basics.py
@@ -0,0 +1,20 @@
+# basics.py
+# This file contains very basic Python concepts.
+
+# Printing output
+print("Hello, world!")
+
+# Variables
+x = 5
+y = 10
+print("Sum:", x + y)
+
+# Conditional statement
+if x < y:
+    print("x is smaller than y")
+else:
+    print("x is greater than or equal to y")
+
+# Loop
+for i in range(3):
+    print("Iteration:", i)
