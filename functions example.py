fa949dc58d9ca4a780165b4520d307b7194f3eed
hemantfoundations <mishrahemantkumar777@gmail.com>
2026-01-25 11:51:12 +0530

    practice done  for this weekend

---
+++ functions example.py
@@ -0,0 +1,33 @@
+# ADDITION
+def add(a, b):
+    return a + b
+
+print(add(2, 3))    # Output: 5
+print(add(10, 5))   # Output: 15
+
+
+# SUBTRACTION
+def subtract(a, b):
+    return a - b
+
+print(subtract(10, 4))   # Output: 6
+print(subtract(7, 12))   # Output: -5
+
+
+# MULTIPLICATION
+def multiply(a, b):
+    return a * b
+
+print(multiply(3, 4))    # Output: 12
+print(multiply(6, 7))    # Output: 42
+
+
+# DIVISION
+def divide(a, b):
+    return a / b
+
+print(divide(10, 2))     # Output: 5.0
+print(divide(7, 2))      # Output: 3.5
+
+
+
